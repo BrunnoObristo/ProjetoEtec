3df36707da67e8ed2041bb8c5c9f9995b3ff839e
marcos paulo <marcos.mpmf43@gmail.com>
2019-05-03 22:03:25 -0300

    Projeto Cadastro

---
+++ src/main/java/com/projetojava/product/ProductApplication.java
@@ -0,0 +1,23 @@
+package com.projetojava.product;
+
+import javax.faces.webapp.FacesServlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean<FacesServlet> serveletResgistrationBean() {
+			return new ServletRegistrationBean<FacesServlet>(new FacesServlet(), "*.xhtml");
+			
+	}
+
+}
