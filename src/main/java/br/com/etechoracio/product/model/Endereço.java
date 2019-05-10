a62a34071f32e1e432d87322f1d9d4ca5000d148
BrunnoObristo <ranggab38@gmail.com>
2019-05-10 19:43:36 -0300

    Banco de dados ok

--- src/main/java/br/com/etechoracio/product/model/Endereço.java
+++ src/main/java/br/com/etechoracio/product/model/Endereço.java
@@ -1,10 +1,17 @@
 package br.com.etechoracio.product.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 @Table(name="ENDER_ETEC")
 public class Endereço {
 
--- src/main/java/br/com/etechoracio/product/model/Escola.java
+++ src/main/java/br/com/etechoracio/product/model/Escola.java
@@ -4,11 +4,19 @@ package br.com.etechoracio.product.model;
 import java.sql.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 
 @Table(name="ETEC")
 public class Escola {
